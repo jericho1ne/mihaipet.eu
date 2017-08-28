4fab6708c067e132c3d8c83b34e54db78fc0b600
Mihai Peteu <jericho1ne@yahoo.com>
2017-08-27 23:34:44 -0700

    remove

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-rsync -v -i -r dist/css/* mpeteu@uzabl.com@rr03.sol.apisnetworks.com:/home/mpeteu/mainwebsite_html/mihaipeteu/css
-rsync -v -i -r dist/*.{html,png,ico} mpeteu@uzabl.com@rr03.sol.apisnetworks.com:/home/mpeteu/mainwebsite_html/mihaipeteu/
