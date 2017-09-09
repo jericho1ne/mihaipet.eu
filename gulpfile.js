ff75263579904ef1f493cecaaf9967b1f5a72d9d
Mihai Peteu <jericho1ne@yahoo.com>
2017-09-08 18:43:57 -0700

    almost...

--- gulpfile.js
+++ gulpfile.js
@@ -52,10 +52,14 @@ function cleanPath(path) {
 	return del(path);
 }
 
-// Delete everything in destination directory
-gulp.task('clean', function (cb) {
-	return del(`${basePaths.dest}/**/*`, cb);
+// Clean tasks for various purposes
+gulp.task('clean-css', function (cb) {
+	return del(`${paths.styles.dest}/**/*`, cb);
 });
+gulp.task('clean-html', function (cb) {
+	return del(`${basePaths.dest}/**/*.html`, cb);
+});
+
 
 // Inject the page header and footer, copy them to destination folder
 gulp.task('headerfooter', function () {
@@ -72,16 +76,13 @@ gulp.task('headerfooter', function () {
 
 // Page-specific meta tags (title, description, etc)
 // Must wait on headerfooter task to complete
-gulp.task('inject-tags', ['clean', 'headerfooter'], function () {
+gulp.task('inject-tags', ['clean-html', 'headerfooter'], function () {
 	var metaTags = JSON.parse(fs.readFileSync(`${paths.partials}/metatags.json`));
 
 	// Inject page titles and meta description tags
 	for (var key in metaTags) {
 		var destinationFile = `${basePaths.dest}/${metaTags[key]['file']}.html`;
-
 		gutil.log(`\t Adding tags to ` + gutil.colors.green(`[ ${destinationFile} ]` ));
-
-		// gulp-template: Inject JSON into HTML header
 		gulp.src(destinationFile)
 			.pipe(template({
 				title: metaTags[key]['title'],
@@ -91,34 +92,62 @@ gulp.task('inject-tags', ['clean', 'headerfooter'], function () {
 	} // End for loop through meta tags
 });
 
-// Create project pages on the fly
-gulp.task('project-templates', ['inject-tags'], function () {
+// Create project pages
+gulp.task('project-pages', function () {
 	var projects = JSON.parse(fs.readFileSync(`${paths.partials}/projects.json`));
-	var sourceTemplate = fs.readFileSync(`${paths.partials}/project-template.html`);
-
-	var templateCard = `${paths.partials}/card-template.html`;
+	//var sourceTemplate = fs.readFileSync(`${paths.partials}/project-template.html`);
 	var templateProject = `${paths.partials}/project-template.html`;
+	var projectDest = `${paths.project.dest}`;
+
+	var promises = Object.keys(projects).map(function (key) {
+        var deferred = Q.defer();
+        var proj = projects[key];
+
+        projectFilename = `${proj['slug']}.html`;
+		destinationProject = `${projectDest}/${projectFilename}`;
+
+
+		// Go over each of those files, and inject project details
+		gutil.log(`\t Creating ` + gutil.colors.green(`[${projectFilename}]` ));
+
+		gulp.src(templateProject)
+			.pipe(template({
+				slug: projects[key]['slug'],
+				title: projects[key]['title'],
+				subtitle: projects[key]['subtitle'],
+				detail: projects[key]['detail'],
+				media: '',
+			}))
+			// rename to eg: `fifa-gate.html`
+			.pipe(rename(projectFilename))
+			// output file above to `/dist/projects/`
+			.pipe(gulp.dest(paths.project.dest));
+
+		gutil.log(`\t destinationProject ` + gutil.colors.green(`[${destinationProject}]` ));
+	  	return deferred.promise;
+    });
+    return Q.all(promises);
+});
+// Create project cards on the fly
+gulp.task('project-cards', function () {
+	var projects = JSON.parse(fs.readFileSync(`${paths.partials}/projects.json`));
+	var templateCard = `${paths.partials}/card-template.html`;
 
 	var projectFilename = '';
-	var tempCardHtml = '';
-	var cardPaths = [];
+	var destinationCard = '';
 
-	var promises = [];
-
+	/*
+	 * Create individual project card HTML files while appending their contents
+	 * to an HTML string for the parent `projects.html` page.
+	 */
  	var promises = Object.keys(projects).map(function (key) {
         var deferred = Q.defer();
         var proj = projects[key];
 
 		projectFilename = `${proj['slug']}.html`;
-		projectTempCard = `card-${projectFilename}`;
+        destinationCard = `${paths.project.dest}/card-${projectFilename}`;
 
-        gutil.log(`\tProcessing ` + gutil.colors.green(`[${proj['slug']}]` ));
 
-        var fullCardPath = `${paths.project.dest}/card-${projectFilename}`;
-
-        //gutil.log('\t' + fullCardPath);
-
-        // Create temporary individual project card HTML files
 		gulp.src(templateCard)
 			.pipe(template({
 				slug: proj['slug'],
@@ -128,67 +157,39 @@ gulp.task('project-templates', ['inject-tags'], function () {
 				button: proj['button'],
 				link: proj['link'],
 			}))
-			.pipe(rename(projectTempCard))
+			.pipe(rename('card-' + projectFilename))
 			.pipe(gulp.dest(`${paths.project.dest}`))
 			.pipe(through.obj(function (chunk, enc, cb) {
 				// Append project card HTML source
-				cardsHtmlSource += fs.readFileSync(fullCardPath, { encoding: 'utf-8' });
+				cardsHtmlSource += fs.readFileSync(destinationCard, { encoding: 'utf-8' });
                 deferred.resolve();
 			}));
-
-		// Use template project to create dummy project files
-		// (eg:  /dist/projects/fifa-gate.html)
-		gulp.src(templateProject)
-			.pipe(rename(projectFilename))
-			.pipe(gulp.dest(paths.project.dest));
-
-		console.log(`${paths.project.dest}/${projectFilename}`);
-		// Go over each of those files, and inject project details
-		gulp.src(`${paths.project.dest}/${projectFilename}`)
-			.pipe(template({
-				slug: projects[key]['slug'],
-				title: projects[key]['title'],
-				subtitle: projects[key]['subtitle'],
-				detail: projects[key]['detail'],
-				media: '',
-			}))
-			.pipe(gulp.dest(paths.project.dest));
 
         return deferred.promise;
     });
     return Q.all(promises);
-
-
-	/*
-	 * Create individual project HTML files, while building up array of project cards
-	 * for parent Projects page.
-	 */
-
-	/*
-	for (var key in projects) {
-		// cardPaths.push(fullCardPath);
-	} // End for loop through projects.json
-	*/
 });
 
-gulp.task('projects-page', ['project-templates'], function () {
-	var sourceFile = `${basePaths.dest}/projects.html`;
-	gutil.log(`\tInjecting all the project cards into ` + gutil.colors.green(sourceFile));
+gulp.task('projects-parent', ['project-cards'], function () {
+	gutil.log(`\tInjecting all the project cards into ` +
+		gutil.colors.green(`${basePaths.dest}/projects.html`));
 
 	// Inject project details into project pages, delete temp card files when done
-	gulp.src(sourceFile)
+	gulp.src(`${basePaths.dest}/projects.html`)
 		.pipe(template({
 			projects: cardsHtmlSource,
 		}))
-		.pipe(gulp.dest(`${basePaths.dest}`))
+		.pipe(gulp.dest(`${basePaths.dest}`));
+		/*
 		.on('end', function() {
 			cleanPath('dist/project/card-*.html');
 		});
+		*/
 });
 
 
 // Compile SASS into CSS, auto-injecting changed files into browser
-gulp.task('sass', function() {
+gulp.task('sass', ['clean-css'], function() {
 	return gulp.src(`${paths.styles.src}/styles.scss`)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(paths.styles.dest))
@@ -213,7 +214,7 @@ gulp.task('minify-css', ['sass'], function() {
 });
 
 // Copy lib libraries from /node_modules into /lib
-gulp.task('copy', ['clean'], function() {
+gulp.task('copy', function() {
 	gutil.log(`>> Started ` + gutil.colors.green(`copy`) + ` task`);
 
 	// Copy media files
@@ -255,7 +256,6 @@ gulp.task('serve',
 		'minify-js',
 		'copy',
 		'inject-tags',
-		'project-templates'
 	], function() {
 		// Initialize a local server in the styleguide root directory
 		browserSync.init({
